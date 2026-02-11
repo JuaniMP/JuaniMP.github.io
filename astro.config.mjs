4fbef4ca21b9b711871074cbe15386e7b3c2fff7
Juanita Mejia <150541532+JuaniMP@users.noreply.github.com>
2026-02-11 10:46:47 -0500

    holi

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,17 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
+// ❌ Eliminamos el import de Netlify
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://JuaniMP.github.io",
+  // base: '/nombre-de-tu-repo', // 👈 Descomenta esto si tu repo NO se llama "JuaniMP.github.io"
   output: 'static',
   integrations: [mdx(), sitemap(), icon()],
-  adapter: netlify(),
+  // ❌ Eliminamos la línea de adapter: netlify(),
   vite: {
-    // @ts-ignore
     plugins: [tailwindcss()],
   },
 });
